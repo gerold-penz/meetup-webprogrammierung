38a84055d3bdabea6464e6143c364011daf28652
Gerold Penz <gerold@halvar.at>
2018-11-22 22:29:19 +0100

    Meetup von 2018-11-21

---
+++ meetups/2018-11-21/beispiel/erstes-programm.js
@@ -0,0 +1,13 @@
+function showMessage() {
+    alert("Hallo Oberhofen")
+}
+
+
+function welcomePart1() {
+    document.getElementById("anzeige").innerText = "Willkommen ..."
+}
+
+
+function welcomePart2() {
+    document.getElementById("anzeige").innerText = "... in Oberhofen."
+}
