5dc7e3a6166538a33bc0603c84bfc5506f5d1001
Gerold Penz <gerold@halvar.at>
2018-12-03 21:07:15 +0100

    ...

--- meetups/2018-12-04/vorbereitungen/taschenrechner.js
+++ meetups/2018-12-04/vorbereitungen/taschenrechner.js
@@ -3,7 +3,7 @@
 let calculation = ""
 let display = document.getElementById("display")
 
-
+ 
 function showResult() {
 
     let result = ""
@@ -11,16 +11,17 @@ function showResult() {
     if (calculation.includes("=")) {
         // "="-Zeichen entfernen und berechnen
         result = calculation + eval(calculation.slice(0, -1))
+        result = result.replace("=", " = ")
     } else {
         result = calculation
     }
 
     // Anzeigen
-    display.innerText = result
+    display.innerHTML = result
 }
 
 
-function addChar(char) {
+function addChars(chars) {
 
     // Verhindern, dass noch ein "=" hinzugefügt wird
     if (calculation.includes("=")) {
@@ -28,8 +29,24 @@ function addChar(char) {
     }
 
     // Zeichen hinzufügen
-    calculation = calculation + char
+    calculation = calculation + chars
 
     // Ergebnis anzeigen
+    showResult()
+}
+
+
+function deleteAll() {
+    calculation = ""
+    showResult()
+}
+
+
+function deleteLast() {
+    // Verhindern, dass gelöscht wird
+    if (calculation.includes("=")) {
+        return
+    }
+    calculation = calculation.slice(0, -1)
     showResult()
 }
