62d85ab0dcee1c0458e53256df2f17d5b1f248b6
Gerold Penz <gerold@halvar.at>
2018-12-11 21:07:11 +0100

    Meetup 2018-12-11

--- meetups/2018-12-11/variablen_skript1.js
+++ meetups/2018-12-11/variablen_skript1.js
@@ -1,1 +1,16 @@
 // JavaScript 1
+(function() {
+    "use strict"
+    
+    const PI = 3.14
+    const BASE_URL = "http://halvar.at"
+
+
+    function halloWelt() {
+        const hallo = "Servus in Oberhofen 1"
+        console.log(hallo)
+        console.log(BASE_URL)
+    }
+    halloWelt()
+
+})()
--- meetups/2018-12-11/variablen_skript2.js
+++ meetups/2018-12-11/variablen_skript2.js
@@ -1,1 +1,41 @@
 // JavaScript 2
+
+(function() {
+    "use strict"
+
+    const PI = 3.14
+    const BASE_URL = "http://halvar.at"
+
+
+    function halloWelt() {
+        let myText = "Hallo Welt"
+
+        myText = "Servus"
+        console.log(myText.length)
+    }
+    // halloWelt()
+
+    
+    function servus() {
+        let myNumber = 12345
+        console.log(myNumber)
+    }
+    // servus()
+
+
+    function xxx() {
+
+        let myBoolean = true
+        console.log(myBoolean)
+    }
+    // xxx()
+
+
+    function objekttypen() { 
+        let myArray = [1, 2, ["Gerold", 3], 4, 5]
+        console.log(myArray[2][1])
+    }
+    objekttypen()
+
+
+})()
