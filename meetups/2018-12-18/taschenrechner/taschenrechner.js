d61f76b6f2d2cecc4410021eaf8826c137504362
Gerold Penz <gerold@halvar.at>
2018-12-18 21:03:29 +0100

    Meetup 2018-12-18

--- meetups/2018-12-18/taschenrechner/taschenrechner.js
+++ meetups/2018-12-18/taschenrechner/taschenrechner.js
@@ -1,3 +1,8 @@
+"use strict"
+
+const BACKGROUND_IMAGE_VALUE = "background_image_value"
+const backgroundImageChanger = document.getElementById("backgroundImageChanger")
+
 let calculation = ""
 let display = document.getElementById("display")
 
@@ -15,6 +20,54 @@ function showResult() {
 
 
 function addChar(char) {
-    calculation = calculation + char
+    if (calculation === "0") {
+        calculation = char
+    } else {
+        calculation = calculation + char
+    }
     showResult()
 }
+
+
+function deleteLastChar() {
+    if (calculation.includes("=")) {
+        return
+    }
+    if (calculation === "0") {
+        return
+    }
+    calculation = calculation.slice(0, -1)
+    if (calculation === "") {
+        calculation = "0"
+    }
+    showResult()
+}
+
+
+function deleteAll() {
+    calculation = "0"
+    showResult()
+}
+
+
+function changeBackgroundImage() {
+
+    const calculator = document.getElementById("calculator")
+    const backgroundValue = backgroundImageChanger.value
+    calculator.style.backgroundImage = backgroundValue
+
+    localStorage.setItem(BACKGROUND_IMAGE_VALUE, backgroundValue)
+}
+
+
+function loadBackgroundImage() {
+    const backgroundValue = localStorage.getItem(BACKGROUND_IMAGE_VALUE)
+    if (!backgroundValue) {
+        return
+    }
+
+    calculator.style.backgroundImage = backgroundValue
+    backgroundImageChanger.value = backgroundValue
+}
+loadBackgroundImage()
+
