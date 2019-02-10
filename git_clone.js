de5f8369ab1d98bdbc595656f7e2e9a5f401821c
Gerold Penz <gerold@halvar.at>
2019-02-10 11:26:41 +0100

    ...

---
+++ git_clone.js
@@ -0,0 +1,15 @@
+// Erstellt im Ordner dieser Datei einen Klon des Git-Repositories unseres Meetups.
+    
+(function() {
+    "use strict"
+
+    const child_process = require("child_process");
+    
+    
+
+
+    
+
+    
+
+})()
