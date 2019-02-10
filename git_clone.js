6e5c32fb4e9cc2e424b953bf761f9db54673d5b6
Gerold Penz <gerold@halvar.at>
2019-02-10 11:43:41 +0100

    Tests mit node.js und child_process

--- git_clone.js
+++ git_clone.js
@@ -1,15 +1,13 @@
 // Erstellt im Ordner dieser Datei einen Klon des Git-Repositories unseres Meetups.
-    
-(function() {
+
+const child_process = require("child_process");
+
+
+(function () {
     "use strict"
 
-    const child_process = require("child_process");
-    
-    
-
-
-    
-
-    
+    child_process.exec("git pull", (error, stdout, stderr) => {
+        console.log(stdout)
+    })
 
 })()
